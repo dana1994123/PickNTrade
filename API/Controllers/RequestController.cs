7aec1e3c6fc4bc7c66decf34a6ed44a9ba97f057
abdelmhe <92818538+abdelmhe@users.noreply.github.com>
2022-12-17 14:10:21 -0500

    Added RequestController

--- API/Controllers/RequestController.cs
+++ API/Controllers/RequestController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,6 +13,44 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class RequestController : ControllerBase
     {
+
+        private Database _database;
+
+        public  RequestController(Database database){
+            _database = database; 
+        }
+
+        //get requests for specific product 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getRequestsForSpecificProductsAsync(Guid id ){
+            try{
+                var requestsForProduct = await _database.Products.FindAsync(id);
+                return Ok(requestsForProduct);
+            }
+             catch(System.Exception){
+             return BadRequest();
+            }
+
+        }
+        //add a request for a specific product
+        [HttpPost("{id}")]
+        public async Task<IActionResult> AddRequestToProductAsync(string id , Request r ){
+            try{
+                var product = await _database.Products.Include(x=>x.Requests).SingleOrDefaultAsync(x=> x.Id == new Guid(id));
+
+                if(product == null){
+                    return NotFound("No product found");
+                }
+
+                product.Requests.Add(r);
+                await _database.SaveChangesAsync();
+                return Ok("");
+            }
+             catch(System.Exception){
+                return BadRequest();
+            }
+
+        }
         
     }
 }
